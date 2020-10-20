7d29c79ed0fc778874cd8f66a830c29776cd9a25
sdfxisme <sdfx@yandex.ru>
2020-10-20 14:23:30 +0300

    ок

---
+++ teneleven/news/templatetags/news_tags.py
@@ -0,0 +1,18 @@
+from django import template
+from news.models import Category
+from django.db.models import Count, F
+from django.core.cache import cache
+
+register = template.Library()
+
+@register.simple_tag(name = 'get_list_categories')
+def get_categories():
+    return Category.objects.all()
+
+@register.inclusion_tag('news/list_categories.html')
+def show_categories(arg1 ='hello', arg2 = 'world'):
+    categories = cache.get('categories')
+    if not categories:
+        categories = Category.objects.annotate(cnt=Count('news', filter=F('news__is_published'))).filter(cnt__gt=0)
+        cache.set('categories', categories, 30)
+    return {"categories": categories, 'arg1':arg1, 'arg2': arg2}
